dc01791ce35519af9e18a1cf6d904c7d3ce3ee34
Alexey Levashev <alexey.levashev@tl-istu.com>
2021-01-17 20:14:49 +0800

    Исправил условие выбора (вложение условий if)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -35,13 +35,15 @@ void MainWindow::on_button_Submit_clicked()
 
     AccountModel account = db.SelectAccount(login, password);
 
-    if (account.id > 0 and account.role!="admin") {
-        ui->statusbar->showMessage("Вход разрешён");
-        accountInfo = new AccountInfo(this, account.id);
-        hide();
-        accountInfo->show();}
-   else if (account.id > 0 and account.role=="admin") {
-        ui->statusbar->showMessage("Тут будет другое окно");
-    }
-   else {ui->statusbar->showMessage("Вход запрещён");}
+    if (account.id > 0) {
+        if (account.role=="admin") {
+            ui->statusbar->showMessage("Тут будет другое окно");
+        }
+        else {
+            ui->statusbar->showMessage("Вход разрешён");
+            accountInfo = new AccountInfo(this, account.id);
+            hide();
+            accountInfo->show();}
+        }
+    else {ui->statusbar->showMessage("Вход запрещён");}
 }
